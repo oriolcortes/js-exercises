7fd190ef767c726bdbbf06970d89ae20f930715a
oriolcortes <oriol.cortesc@gmail.com>
2025-05-15 12:01:23 +0200

    chore/update comments and add require true

--- setup-gpg-filters.sh
+++ setup-gpg-filters.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Afegir o actualitzar filters gpgC1–gpgC6 en .git/config
+# Añadir o actualizar filters gpgC00–gpgC06 a .git/config
 set -euo pipefail
 
 PREFIX="session"
-# Defineix el número d'iteracions (per exemple, 0 per session00.pass; canvia aquest valor per processar més fitxers)
+# Define el número de iteraciones (por ejemplo, 0 para session00.pass; cambia este valor para procesar más ficheros)
 END=0
 LAST_ID=""
 counter=0
@@ -12,7 +12,8 @@ for i in $(seq 0 $END); do
   LAST_ID=$(printf "%02d" "$i")
   git config --local filter.gpgC${LAST_ID}.clean "gpg --batch --yes --passphrase-file .keys/${PREFIX}${LAST_ID}.pass -c -o -"
   git config --local filter.gpgC${LAST_ID}.smudge "gpg --batch --yes --passphrase-file .keys/${PREFIX}${LAST_ID}.pass -d -o -"
+  git config --local filter.gpgC${LAST_ID}.required "true"
   counter=$((counter+1))
 done
 
-echo "Filters configurats per a $counter fitxer(s) fins a ${PREFIX}${LAST_ID}.pass"
+echo "Filters configurados para $counter sessions hasta ${PREFIX}${LAST_ID}.pass"
